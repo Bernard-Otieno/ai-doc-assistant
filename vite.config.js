196f95f9eafa8cb4ba45744dc631a4863b3069e3
Bernard Otieno <bernard.otieno@strathmore.edu>
2025-05-07 11:24:01 +0300

    vite file edit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.languagetool.org/v2', // Your API URL
+        target: 'https://api.languagetool.org/v2', //  API URL
         changeOrigin: true,
-        secure: false, // If using HTTPS API
+        secure: false, // using HTTPS API
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
